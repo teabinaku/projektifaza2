291669c6f8c1a4d679717af3343500ce93526455
teabinaku <119872321+teabinaku@users.noreply.github.com>
2023-01-22 13:44:08 +0100

    updated

--- login/signup.js
+++ login/signup.js
@@ -3,11 +3,25 @@ function auth(event) {
 
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+    var nrtel = document.getElementById("nrtel").value;
+
+    var shteti = document.getElementById("shteti").value;
+
+    var emri = document.getElementById("emri").value;
+    var mbiemri = document.getElementById("mbiemri").value;
+    var mosha = document.getElementById("mosha").value;
+
+
+
+
+
 
     if (email==="" || password==="" || password.length<8) {
          alert('Ju nuk i keni plotesu te gjitha kushtet');
          return;
          
+   
+     
     } else {
         
         window.location.replace("file:///Users/apple/Desktop/Projekt/Main%20page/index.html");
