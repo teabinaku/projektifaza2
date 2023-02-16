a32670f959e9d290ac9f7be674a7169d6458efd9
teabinaku <119872321+teabinaku@users.noreply.github.com>
2023-02-16 16:43:33 +0100

    hello

---
+++ login/signunp.php
@@ -0,0 +1,15 @@
+<?php
+
+
+ $emri=$_POST['emri'];
+ $mbiemri=$_POST['mbiemri'];
+ $username=$_POST['username'];
+ $password=$_POST['password'];
+ $email=$_POST['email'];
+ $shteti=$_POST['shteti'];
+
+
+
+ $host="localhost";
+ 
+?>
