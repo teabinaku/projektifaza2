8bf1acccd78f580d2cfdb7b36fe044bf4eec367f
teabinaku <119872321+teabinaku@users.noreply.github.com>
2023-02-24 13:23:52 +0100

    e shtova edhe ni sen

--- Main page/index1.php
+++ Main page/index1.php
@@ -88,7 +88,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./dekaaa.jpeg" alt="">
                             <span class="movie-list-item-title">Avatar</span>
                             <p class="movie-list-item-desc">Jake Sully and Ney'tiri have formed a family and are doing everything to stay together. However, they must leave their home and explore the regions of Pandora. When an ancient threat resurfaces, Jake must fight a difficult war against the humans.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -100,7 +100,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./holidate.webp" alt="">
                             <span class="movie-list-item-title">Holidate</span>
                             <p class="movie-list-item-desc">Fed up with being single on holidays, two strangers agree to be each other's platonic plus-ones all year long, only to catch real feelings along the way.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -112,7 +112,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./notebook.jpg" alt="">
                             <span class="movie-list-item-title">The Notebook</span>
                             <p class="movie-list-item-desc">In 1940s South Carolina, mill worker Noah Calhoun (Ryan Gosling) and rich girl Allie (Rachel McAdams) are desperately in love. But her parents don't approve...</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -124,7 +124,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./godfather.jpeg" alt="">
                             <span class="movie-list-item-title">The Godfather</span>
                             <p class="movie-list-item-desc">Widely regarded as one of the greatest films of all time, this mob drama, based on Mario Puzo's novel of the same name, focuses on the powerful Italian-American crime family of Don Vito Corleone (Marlon Brando)</p>
-                            <<form action="../profili/profilii.php">
+                            <<form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -137,7 +137,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./black panther.jpeg" alt="">
                             <span class="movie-list-item-title">Black Panther</span>
                             <p class="movie-list-item-desc">Queen Ramonda, Shuri, M'Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T'Challa's death. As the Wakandans strive to embrace their next chapter, the heroes must band together with Nakia and Everett Ross to forge a new path for their beloved kingdom.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -167,7 +167,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./thor.jpeg" alt="">
                             <span class="movie-list-item-title">Thor: Love and Thunder</span>
                             <p class="movie-list-item-desc">Thor embarks on a journey unlike anything he's ever faced -- a quest for inner peace. However, his retirement gets interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -179,7 +179,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./doctorstrange.jpeg" alt="">
                             <span class="movie-list-item-title">Doctor Strange in the Multiverse of Madness</span>
                             <p class="movie-list-item-desc"></p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -200,7 +200,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./dontworrt.jpeg" alt="">
                             <span class="movie-list-item-title">Don't Worry Darling</span>
                             <p class="movie-list-item-desc">In the 1950s, Alice and Jack live in the idealized community of Victory, an experimental company town that houses the men who work on a top-secret project.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -212,7 +212,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./Black_Adam_-_Infobox_Image.webp" alt="">
                             <span class="movie-list-item-title">Black Adam</span>
                             <p class="movie-list-item-desc">In ancient Kahndaq, Teth Adam was bestowed the almighty powers of the gods. After using these powers for vengeance, he was imprisoned, becoming Black Adam.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -242,7 +242,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./crazy rich asians.jpeg" alt="">
                             <span class="movie-list-item-title">Crazy Rich Asians</span>
                             <p class="movie-list-item-desc">Rachel Chu is happy to accompany her longtime boyfriend, Nick, to his best friend's wedding in Singapore. She's also surprised to learn that Nick's family is extremely wealthy and he's considered one of the country's most eligible bachelors..</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -263,7 +263,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./howtoloseaguy.jpeg" alt="">
                             <span class="movie-list-item-title">How to lose a guy in 10 days</span>
                             <p class="movie-list-item-desc">An advice columnist, Andie Anderson (Kate Hudson), tries pushing the boundaries of what she can write about in her new piece about how to get a man to leave you in 10 days. Her editor, Lana (Bebe Neuwirth), loves it, and Andie goes off to find a man she can use for the experiment.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -275,7 +275,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./lalaland.jpeg" alt="">
                             <span class="movie-list-item-title">La la Land</span>
                             <p class="movie-list-item-desc">Sebastian (Ryan Gosling) and Mia (Emma Stone) are drawn together by their common desire to do what they love. But as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -287,7 +287,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./ATONOMENT.jpeg" alt="">
                             <span class="movie-list-item-title">Atonement</span>
                             <p class="movie-list-item-desc">This sweeping English drama, based on the book by Ian McEwan, follows the lives of young lovers Cecilia Tallis (Keira Knightley) and Robbie Turner (James McAvoy).</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -318,7 +318,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./predator.jpeg" alt="">
                             <span class="movie-list-item-title">Predator</span>
                             <p class="movie-list-item-desc">Dutch (Arnold Schwarzenegger), a soldier of fortune, is hired by the U.S. government to secretly rescue a group of politicians trapped in Guatemala.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -330,7 +330,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./badboys.jpeg" alt="">
                             <span class="movie-list-item-title">Bad Boys</span>
                             <p class="movie-list-item-desc">Miami-Dade detectives Mike Lowrey (Will Smith) and Marcus Burnett (Martin Lawrence) blow a fuse when $100 million worth of heroin they recently confiscated is heisted from station headquarters.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -342,7 +342,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./topmaverick.jpeg" alt="">
                             <span class="movie-list-item-title">Top Gun: Maverick</span>
                             <p class="movie-list-item-desc">After more than 30 years of service as one of the Navy's top aviators, Pete "Maverick" Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
@@ -354,7 +354,7 @@ $images = array(
                             <img class="movie-list-item-img" src="./fastfurious.webp " alt="">
                             <span class="movie-list-item-title">Fast & Furious</span>
                             <p class="movie-list-item-desc">Fast & Furious is a media franchise centered on a series of action films that are largely concerned with street racing, heists, spies, and family.</p>
-                            <form action="../profili/profilii.php">
+                            <form action="../watchlater/watchlater.php">
                              <button class="movie-list-item-button">Watch Later</button>
                                </form>
                              <form action="../Comingsoon/comingsoon.php">
